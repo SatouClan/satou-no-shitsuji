69f3cf2406d021ba599fbd1bd2b009f5050808e4
ErrorDIM <satoufukumii@gmail.com>
2022-08-21 12:12:09 +0700

    🪕

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,62 +1,69 @@
-import { CacheType, Client, Interaction } from 'discord.js'  /** discord classes */
+import {
+    CacheType,
+    Client,
+    Interaction,
+} from "discord.js" /** discord classes */
 
 /** messages */
-
+import EXPRESSION_MAP from "./messages/expression"
 
 /** commands */
-import ping from './collections/ping'
-import calc from './collections/calc'
-
+import ping from "./collections/ping"
+import calc from "./collections/calc"
 
 /** deploy */
-import deploy from './deploy'
-
+import deploy from "./deploy"
 
 /** main function */
 export function run(client: Client<boolean>): void {
     deploy() /** register commands */
 
-
     /** on interaction */
-    client.on('interactionCreate', async function (interaction: Interaction<CacheType>) {
-        if (!interaction.isCommand()) return
+    client.on(
+        "interactionCreate",
+        async function (interaction: Interaction<CacheType>) {
+            if (!interaction.isCommand()) return
 
+            try {
+                switch (interaction.commandName) {
+                    case ping.name:
+                        await ping.execute(client, interaction)
+                        break
 
-        try {
-            switch (interaction.commandName) {
-                case ping.name:
-                    await ping.execute(client, interaction)
-                    break
+                    case calc.name:
+                        await calc.execute(interaction)
+                        break
 
-
-                case calc.name:
-                    await calc.execute(interaction)
-                    break
-
-
-                default: break
+                    default:
+                        break
+                }
+            } catch (e) {
+                await interaction.reply("error")
             }
-        } catch (e) { await interaction.reply("error") }
-    })
-
+        }
+    )
 
     /** on message */
-    client.on('messageCreate', async function (message) {
+    client.on("messageCreate", async function (message) {
         const { content } = message
-
 
         /** goodbye and welcome messages */
         if (message.author.id !== client.user?.id) {
-            if (content.toLowerCase().startsWith('welcome to our family'))
-                await message.channel.send('Welcome to our family. Please, enjoy freely with in restriction.')
+            if (content.toLowerCase().startsWith("welcome to our family"))
+                await message.channel.send(
+                    "Welcome to our family. Please, enjoy freely with in restriction."
+                )
 
-            if (content.toLowerCase().startsWith('see you'))
-                await message.channel.send('See you.')
+            if (content.toLowerCase().startsWith("see you"))
+                await message.channel.send("See you.")
+
+            for (const exp in EXPRESSION_MAP)
+                if (content.toLowerCase().includes(exp))
+                    await message.channel.send(EXPRESSION_MAP[exp])
         }
-
 
         /** on mention */
         if (message.mentions.has(client.user?.id!))
-            await message.channel.send('Available.')
+            await message.channel.send("Available.")
     })
 }
---
+++ src/commands/messages/expression.ts
@@ -0,0 +1,7 @@
+const EXPRESSION_MAP: Record<string, string> = {
+    nani: "N-NANI!!!",
+    fuck: "Language.",
+    mlem: "Ahem.",
+}
+
+export default EXPRESSION_MAP
